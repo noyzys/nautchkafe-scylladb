9fc4a99ca8833ef907bc0383aef025cec76356bc
noyzys <damian.festowski@onet.pl>
2024-12-16 16:55:50 +0100

    reduce xql instructions from placeholder

--- src/main/java/dev/nautchkafe/scylladb/bridge/ScyllaSqlConstants.java
+++ src/main/java/dev/nautchkafe/scylladb/bridge/ScyllaSqlConstants.java
@@ -11,4 +11,8 @@ final class ScyllaSqlConstants {
     static final String DELETE_WHERE = "DELETE FROM %s WHERE %s";
     static final String INSERT = "INSERT INTO %s VALUES (%s)";
 
+    static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS %s (%s)";
+    static final String DROP_TABLE = "DROP TABLE IF EXISTS %s";
+    static final String TABLE_EXISTS = "SELECT table_name FROM system_schema.tables WHERE table_name = ?";
+    static final String TRUNCATE_TABLE = "TRUNCATE TABLE %s";
 }
--- src/main/java/dev/nautchkafe/scylladb/bridge/ScyllaTableCoordinator.java
+++ src/main/java/dev/nautchkafe/scylladb/bridge/ScyllaTableCoordinator.java
@@ -1,6 +1,6 @@
 package dev.nautchkafe.scylladb.bridge;
 
-final class ScyllaTableCoordinator implements ScyllaTable {
+public final class ScyllaTableCoordinator implements ScyllaTable {
 
     private final SqlQueryExecutor queryExecutor;
 
@@ -10,26 +10,26 @@ final class ScyllaTableCoordinator implements ScyllaTable {
 
     @Override
     public Try<Void> createTable(final String tableName, final String schema) {
-        final String query = "CREATE TABLE IF NOT EXISTS " + tableName + " (" + schema + ")";
+        final String query = ScyllaSqlConstants.CREATE_TABLE.formatted(tableName, schema);
         return queryExecutor.executeSync(query, List.empty()).map(rs -> null);
     }
 
     @Override
     public Try<Void> dropTable(final String tableName) {
-        final String query = "DROP TABLE IF EXISTS " + tableName;
+        final String query = ScyllaSqlConstants.DROP_TABLE.formatted(tableName);
         return queryExecutor.executeSync(query, List.empty()).map(rs -> null);
     }
 
     @Override
     public Try<Boolean> tableExists(final String tableName) {
-        final String query = "SELECT table_name FROM system_schema.tables WHERE table_name = ?";
+        final String query = ScyllaSqlConstants.TABLE_EXISTS;
         return queryExecutor.executeSync(query, List.of(tableName))
-            .map(rs -> rs.iterator().hasNext());
+                .map(rs -> rs.iterator().hasNext());
     }
 
     @Override
     public Try<Void> truncateTable(final String tableName) {
-        final String query = "TRUNCATE TABLE " + tableName;
+        final String query = ScyllaSqlConstants.TRUNCATE_TABLE.formatted(tableName);
         return queryExecutor.executeSync(query, List.empty()).map(rs -> null);
     }
 }
